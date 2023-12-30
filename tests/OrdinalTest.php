5112c8f2b2ad1c6d0b606a7ae1bfca1cbd1a88e3
kleytondev <akatsukipb12@gmail.com>
2023-12-30 02:28:22 -0300

    feat: creating test to no configured locale

--- tests/OrdinalTest.php
+++ tests/OrdinalTest.php
@@ -9,10 +9,22 @@ class OrdinalTest extends TestCase {
     /** @test
      * @throws OrdinalTextException
      */
-    public function it_will_return_ordinated_number_in_string()
+    public function it_will_return_ordinated_number_in_string(): void
     {
         $ordinalTest = new OrdinalConverter();
 
         $this->assertEquals('CENTÉSIMA PRIMEIRA', $ordinalTest->toWords(101));
+    }
+
+    /**
+     * @throws OrdinalTextException
+     */
+    public function test_will_throw_exception_if_calls_a_no_configured_locale(): void
+    {
+        $ordinalTest = new OrdinalConverter();
+
+        $this->expectException(OrdinalTextException::class);
+
+        $ordinalTest->toWords(101, 'en-US');
     }
 }
